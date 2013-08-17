8b7a1bc3044d689bd1916dbb8cf07c019739faca
Ryan Zezeski <rzezeski@gmail.com>
2013-08-16 22:49:24 -0400

    Add basho bench rollups

---
+++ basho-bench/bin/calc-95-latency.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Use to calculate the 95th percentile latency for a particular run of
+# basho bench.
+#
+# ./calc-95-latency.sh <results-dir>
+#
+# for d in bench-runs-dir/*; do ./calc-95-latency.sh $d; done
+
+RESULTS_DIR=$1
+
+for lat in $RESULTS_DIR/*latencies*
+do
+    # taking average of all the 95th percentiles, divide by 1000 to
+    # convert from microseconds to milli
+    #
+    # drop first 30 seconds and last line to remove outliers
+    echo -n "The mean 95th latency for $lat: "
+    sed -e '1,4d' -e '$d' $lat | \
+        awk -F, '{total += $7 } END { printf("%f\n", (total / NR) / 1000) }'
+done
