2a89a1bbb0b4f6bb0c10fd506468819ded90d196
Ryan Zezeski <ryan@zinascii.com>
2026-01-11 14:26:23 -0500

    track ipv6 calls, improve output format

--- dtrace/squeue-enter-rx.d
+++ dtrace/squeue-enter-rx.d
@@ -8,7 +8,7 @@
  */
 ip_input:entry
 {
-	self->t = 1;
+	self->t = 4;
 }
 
 ip_input:return
@@ -16,9 +16,19 @@ ip_input:return
 	self->t = 0;
 }
 
+ip_input_v6:entry
+{
+	self->t = 6;
+}
+
+ip_input_v6:return
+{
+	self->t = 0;
+}
+
 #define V4_PART_OF_V6(v6) ((v6)._S6_un._S6_u32[3])
 
-squeue_enter:entry /self->t/
+squeue_enter:entry /self->t == 4/
 {
 	this->sqp = args[0];
 	/*
@@ -29,19 +39,42 @@ squeue_enter:entry /self->t/
 	this->ipdst = &V4_PART_OF_V6(this->connp->connua_v6addr.connua_laddr);
 	this->sq_count = this->sqp->sq_count > 1 ? 2 : this->sqp->sq_count;
 
-	@[cpu, arg0, inet_ntoa(this->ipdst), this->sqp->sq_state, arg5,
+	@ip4[cpu, arg0, inet_ntoa(this->ipdst), this->sqp->sq_state, arg5,
+	    this->sq_count] = count();
+}
+
+squeue_enter:entry /self->t == 6/
+{
+	this->sqp = args[0];
+	/*
+	 * The ip module stashes a pointer to the conn_t in b_prev (see
+	 * SET_SQUEUE).
+	 */
+	this->connp = (conn_t *)args[1]->b_prev;
+	this->ip6dst = &this->connp->connua_v6addr.connua_laddr;
+	this->sq_count = this->sqp->sq_count > 1 ? 2 : this->sqp->sq_count;
+
+	@ip6[cpu, arg0, inet_ntoa6(this->ip6dst), this->sqp->sq_state, arg5,
 	    this->sq_count] = count();
 }
 
 END
 {
-	printf("%-4s %-16s %-18s %-10s %-6s %-6s %-8s\n",
+	printf("--- IPV4 -----------------------------------------------\n");
+	printf("%-4s %-18s %-16s %-10s %-6s %-6s %-8s\n",
 	    "CPU", "SQUEUE", "DST IP", "SQ_STATE", "PFLAG", "SQ_CNT", "NUM");
-	printa("%-4d 0x%p %-16s 0x%-8x 0x%-4x %-6u %@-8u\n", @);
+	printa("%-4d 0x%p %-16s 0x%-8x 0x%-4x %-6u %@-8u\n", @ip4);
+	printf("\n");
 
-	printf("\nDecoder Ring:\n");
-	printf("\t0x320	SQS_ILL_BOUND|SQS_POLL_CAPAB|SQS_BOUND\n");
-	printf("\t0x820	SQS_DEFAULT|SQS_BOUND\n");
-	printf("\t0x825	SQS_DEFAULT|SQS_BOUND|SQS_ENTER|SQS_PROC\n");
-	printf("\t0x829	SQS_DEFAULT|SQS_BOUND|SQS_FAST|SQS_PROC\n");
+	printf("--- IPV6 -----------------------------------------------\n");
+	printf("%-4s %-18s %-28s %-10s %-6s %-6s %-8s\n",
+	    "CPU", "SQUEUE", "DST IP", "SQ_STATE", "PFLAG", "SQ_CNT", "NUM");
+	printa("%-4d 0x%p %-28s 0x%-8x 0x%-4x %-6u %@-8u\n", @ip6);
+	printf("\n");
+
+	printf("--- Decoder Ring ---------------------------------------\n");
+	printf("0x320	SQS_ILL_BOUND|SQS_POLL_CAPAB|SQS_BOUND\n");
+	printf("0x820	SQS_DEFAULT|SQS_BOUND\n");
+	printf("0x825	SQS_DEFAULT|SQS_BOUND|SQS_ENTER|SQS_PROC\n");
+	printf("0x829	SQS_DEFAULT|SQS_BOUND|SQS_FAST|SQS_PROC\n");
 }
