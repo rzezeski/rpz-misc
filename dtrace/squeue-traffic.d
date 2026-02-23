96eabdc1cc0109e89748a962323da4a139aea6d1
Ryan Zezeski <ryan@zinascii.com>
2026-02-23 10:09:24 -0500

    add lport to squeue-traffic

--- dtrace/squeue-traffic.d
+++ dtrace/squeue-traffic.d
@@ -12,11 +12,11 @@ squeue-proc-start
 	this->connp = (conn_t *)arg2;
 	this->laddr = inet_ntoa(&V4_PART_OF_V6(
 		    this->connp->connua_v6addr.connua_laddr));
+	this->lport = this->connp->u_port.connu_ports.connu_lport;
 
-
-	@[this->laddr, caller, probefunc, cpu, arg0] = count();
+	@[this->laddr, this->lport, caller, probefunc, cpu, arg0] = count();
 	@stacks[this->laddr, arg0, probefunc, stack()] = count();
-	@unique[this->laddr, arg0] = count();
+	@unique[this->laddr, this->lport, arg0] = count();
 }
 
 END
@@ -25,12 +25,12 @@ END
 	printa(@stacks);
 
 	printf("=== BREAKOUT\n");
-	printf("%-16s %-32s %-16s %-4s %-18s %-8s\n",
-	    "LADDR", "CALLER", "FUNC", "CPU", "SQUEUE", "COUNT");
-	printa("%-16s %-32a %-16s %-4d 0x%p %@-8u\n", @);
+	printf("%-16s %-8s %-32s %-16s %-4s %-18s %-8s\n",
+	    "LADDR", "LPORT", "CALLER", "FUNC", "CPU", "SQUEUE", "COUNT");
+	printa("%-16s %-8u %-32a %-16s %-4d 0x%p %@-8u\n", @);
 
 	printf("\n");
 	printf("=== SUMMARY\n");
-	printf("%-16s %-18s %-8s\n", "LADDR", "SQUEUE", "COUNT");
-	printa("%-16s 0x%p %@-8u\n", @unique);
+	printf("%-16s %-8s %-18s %-8s\n", "LADDR", "LPORT", "SQUEUE", "COUNT");
+	printa("%-16s %-8u 0x%p %@-8u\n", @unique);
 }
